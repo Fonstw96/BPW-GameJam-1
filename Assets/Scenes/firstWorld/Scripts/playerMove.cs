f5e0e399e32bb185cfe687b079bb4c59ea8dfac2
Unknown <fons_met_het_lange_e-mailadres@abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijk.com>
2018-04-23 15:17:33 +0200

    You can go up and down!
    Still needs fixing, though...

--- Assets/Scenes/firstWorld/Scripts/playerMove.cs
+++ Assets/Scenes/firstWorld/Scripts/playerMove.cs
@@ -6,15 +6,17 @@ public class playerMove : MonoBehaviour
 {
     public float maxForwardSpeed = 12;
     public float forwardAcceleration = 2;
-    public float slowDownAcceleration = 1;
+    public float forwardBrake = 1;
     private float currentForwardSpeed = 0;
 
     public float maxSteeringSpeed = 8;
     public float steeringAcceleration = 3;
+    public float steeringBrake = .6667f;
     private float currentSteeringSpeed = 0;
 
     public float maxRaisingSpeed = 8;
     public float raisingAcceleration = 3;
+    public float raisingBrake = .6667f;
     private float currentRaisingSpeed = 0;
 
     void Start ()
@@ -42,7 +44,7 @@ public class playerMove : MonoBehaviour
         }
         else if (currentForwardSpeed > 0 && !Input.GetButton("Jump"))
         {
-            currentForwardSpeed -= slowDownAcceleration * Time.deltaTime;
+            currentForwardSpeed -= forwardBrake * Time.deltaTime;
 
             if (currentForwardSpeed < 0)
                 currentForwardSpeed = 0;
@@ -56,11 +58,54 @@ public class playerMove : MonoBehaviour
             if (currentSteeringSpeed > maxSteeringSpeed)
                 currentSteeringSpeed = maxSteeringSpeed;
         }
+        else if (Input.GetAxis("Horizontal") == 0)
+        {
+            if (currentSteeringSpeed > 0)
+            {
+                currentSteeringSpeed -= steeringBrake * Time.deltaTime;
+
+                if (currentSteeringSpeed < 0)
+                    currentSteeringSpeed = 0;
+            }
+            else if (currentSteeringSpeed < 0)
+            {
+                currentSteeringSpeed += steeringBrake * Time.deltaTime;
+
+                if (currentSteeringSpeed > 0)
+                    currentSteeringSpeed = 0;
+            }
+        }
+
+        // Going up/down
+        if (currentRaisingSpeed < maxRaisingSpeed && Input.GetAxis("Vertical") != 0)
+        {
+            currentRaisingSpeed += Input.GetAxis("Vertical") * raisingAcceleration * Time.deltaTime;
+
+            if (currentRaisingSpeed > maxRaisingSpeed)
+                currentRaisingSpeed = maxRaisingSpeed;
+        }
+        else if (Input.GetAxis("Vertical") == 0)
+        {
+            if (currentRaisingSpeed > 0)
+            {
+                currentRaisingSpeed -= raisingBrake* Time.deltaTime;
+
+                if (currentRaisingSpeed < 0)
+                    currentRaisingSpeed = 0;
+            }
+            else if (currentRaisingSpeed < 0)
+            {
+                currentRaisingSpeed += raisingBrake * Time.deltaTime;
+
+                if (currentRaisingSpeed > 0)
+                    currentRaisingSpeed = 0;
+            }
+        }
     }
 
     private void Move()
     {
         transform.position += transform.forward * currentForwardSpeed;
-        transform.Rotate(0, currentSteeringSpeed, 0);
+        transform.Rotate(currentRaisingSpeed, currentSteeringSpeed, 0);
     }
 }
