e92f6db40fda006f8a8dfa68d7a8faf00e7e0dc9
Unknown <fons_met_het_lange_e-mailadres@abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijk.com>
2018-04-23 14:48:59 +0200

    Player moves forward

---
+++ Assets/Scenes/firstWorld/Scripts/playerMove.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMove : MonoBehaviour
+{
+    public float maxSpeed = 12;
+    public float forwardAcceleration = 2;
+    public float slowDownAcceleration = 1;
+    public float steeringAcceleration = 3;
+    private float currentForwardSpeed = 0;
+
+	void Start ()
+    {
+		
+	}
+
+	void Update ()
+    {
+        GetInput();
+        Move();
+
+        print(currentForwardSpeed);
+	}
+
+    private void GetInput()
+    {
+        // Forward movement
+        if (currentForwardSpeed < maxSpeed && Input.GetAxis("Vertical") > 0)
+        {
+            currentForwardSpeed += Input.GetAxis("Vertical") * forwardAcceleration;
+
+            if (currentForwardSpeed > maxSpeed)
+                currentForwardSpeed = maxSpeed;
+        }
+        else if (currentForwardSpeed > 0 && Input.GetAxis("Vertical") <= 0)
+        {
+            currentForwardSpeed -= slowDownAcceleration;
+
+            if (currentForwardSpeed < 0)
+                currentForwardSpeed = 0;
+        }
+
+        // Steering
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            // steer
+        }
+    }
+
+    private void Move()
+    {
+        transform.position += transform.forward * currentForwardSpeed;
+    }
+}
