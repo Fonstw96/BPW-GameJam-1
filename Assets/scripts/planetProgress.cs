efdaa869dba52900a21c89086e63366fc92344c5
Unknown <fons_met_het_lange_e-mailadres@abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijk.com>
2018-04-23 16:52:50 +0200

    Continued UI, planets show ingredient progression

---
+++ Assets/scripts/planetProgress.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class planetProgress : MonoBehaviour
+{
+    public Text uiProgressText;
+    public int totalIngredients = 7;
+
+	void Start ()
+    {
+		
+	}
+
+	void Update ()
+    {
+		
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            int currentIngredients = other.GetComponent<PlayerCollisionScript>().IntIngredient1;
+            uiProgressText.text = currentIngredients + " / " + totalIngredients;
+        }
+    }
+}
