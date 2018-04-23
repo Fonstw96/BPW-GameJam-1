6881dc2f85c1a32add5d32c7e8a6b751eeaf8da6
Unknown <fons_met_het_lange_e-mailadres@abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijk.com>
2018-04-23 17:31:09 +0200

    You can win now!

---
+++ Assets/scripts/winScreen.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class winScreen : MonoBehaviour
+{
+    public Image[] uiIngredients;
+    public GameObject thethingyouwinwith;
+
+	void Start ()
+    {
+		
+	}
+
+	void Update ()
+    {
+        int completed = 0;
+        foreach (Image ingredient in uiIngredients)
+        {
+            if (ingredient.color == Color.white)
+                completed++;
+        }
+
+        if (completed == uiIngredients.Length)
+            thethingyouwinwith.SetActive(true);
+	}
+}
