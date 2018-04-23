00cdf58729579cbad8d4bbbaa1b9fb2b1549382f
Unknown <melonodev@gmail.com>
2018-04-23 16:24:45 +0200

    Working Pickup

--- Assets/scripts/PlayerCollisionScript.cs
+++ Assets/scripts/PlayerCollisionScript.cs
@@ -4,27 +4,23 @@ using UnityEngine;
 
 public class PlayerCollisionScript : MonoBehaviour {
 
-	public int[] IntIngredient = new int[3];
+	public int IntIngredient1 = 0;
+	public int IntIngredient2 = 0;
 	public GameObject Ingredient1;
 	public GameObject Ingredient2;
 	public Rigidbody rb;
 	private Vector3 planetBounce;
 
-	// Use this for initialization
-	void Start()
-	{
-		IntIngredient [1] = 1;
-		IntIngredient [2] = 1;
-	}
+
 
 	void OnTriggerEnter(Collider other){
 		if (other.gameObject.CompareTag ("Ingredient1")){
-			IntIngredient[1] += 1;
-			print (IntIngredient [1]);
+			IntIngredient1 += 1;
+			print (IntIngredient1);
 		}
 
 		if (other.gameObject.CompareTag ("Ingredient2")){
-			IntIngredient[2] += 1;
+			IntIngredient2 += 1;
 		}
 
 	}
@@ -35,12 +31,12 @@ public class PlayerCollisionScript : MonoBehaviour {
 
 		if (collision.gameObject.CompareTag ("Planet")){
 			//lose materials 
-			if (IntIngredient[1] >= 1) {
-				IntIngredient[1] -= 1;
+			if (IntIngredient1 >= 1) {
+				IntIngredient1 -= 1;
 				Instantiate (Ingredient1, this.transform.position + new Vector3 (3,3,3), this.transform.rotation);
 			}
-			if (IntIngredient[2] >= 1) {
-				IntIngredient[2] -= 1;
+			if (IntIngredient2 >= 1) {
+				IntIngredient2 -= 1;
 				Instantiate (Ingredient2, this.transform.position + new Vector3 (-3,3,-3), this.transform.rotation);
 			}
 
